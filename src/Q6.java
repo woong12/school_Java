04e2094679d42c8ed16279cf32350a0aea204338
woong <koohw0109@gmail.com>
2023-04-13 15:04:03 +0900

    0413testprac

--- src/Q6.java
+++ src/Q6.java
@@ -7,5 +7,14 @@ public class Q6 {
             System.out.println(num1);
         }
         System.out.println(num2);
+
+        System.out.println("----------------------------");
+        int num3 = 3;
+        int num4 = 7;
+
+        if(++num1 < 5 || ++num2 > 8) {
+            System.out.println(num3);
+        }
+        System.out.println(num4);
     }
 }
